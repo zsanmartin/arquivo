56f6f054912ddd811b2de2212e0550b2e8cf27e6
José San Martin <jose@chuva-inc.com>
2023-06-27 11:56:55 -0300

    Corrige script de crop

--- crop-images.sh
+++ crop-images.sh
@@ -2,10 +2,10 @@ for PHOTO_FILEPATH in $(find images -name "*.jpeg"); do
   for THUMB_DIR in thumbs largeThumbs; do
     PHOTO_DIR=${PHOTO_FILEPATH%/*g}
     mkdir -p "$THUMB_DIR/$PHOTO_DIR"
-    $DESTINATION_FILE="$THUMB_DIR/$PHOTO_FILEPATH"
+    DESTINATION_FILE="$THUMB_DIR/$PHOTO_FILEPATH"
     if [ ! -f "$DESTINATION_FILE" ]; then
-      convert -resize 320x320^ -gravity center -extent 300x300 $PHOTO_FILEPATH
+      echo "$THUMB_DIR/$PHOTO_DIR"
+      convert -resize 320x320^ -gravity center -extent 300x300 $PHOTO_FILEPATH $DESTINATION_FILE
     fi
   done
 done
-
