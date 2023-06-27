a2393085e474f33c96e5cc76b17eca549da17174
José San Martin <jose@chuva-inc.com>
2023-06-27 11:36:02 -0300

    Evita regerar crops

--- crop-images.sh
+++ crop-images.sh
@@ -1,6 +1,11 @@
 for PHOTO_FILEPATH in $(find images -name "*.jpeg"); do
-  PHOTO_DIR=${PHOTO_FILEPATH%/*g}
-  mkdir -p "thumbs/$PHOTO_DIR"
-  convert -resize 320x320^ -gravity center -extent 300x300 $PHOTO_FILEPATH "thumbs/$PHOTO_FILEPATH"
+  for THUMB_DIR in thumbs largeThumbs; do
+    PHOTO_DIR=${PHOTO_FILEPATH%/*g}
+    mkdir -p "$THUMB_DIR/$PHOTO_DIR"
+    $DESTINATION_FILE="$THUMB_DIR/$PHOTO_FILEPATH"
+    if [ ! -f "$DESTINATION_FILE" ]; then
+      convert -resize 320x320^ -gravity center -extent 300x300 $PHOTO_FILEPATH
+    fi
+  done
 done
 
