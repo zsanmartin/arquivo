6d6fd6c97585599c7e16fc026ed58343ac7f3e3e
José San Martin <jose@chuva-inc.com>
2023-07-03 21:44:24 -0300

    Regera large thumbs

--- crop-images.sh
+++ crop-images.sh
@@ -4,8 +4,12 @@ for PHOTO_FILEPATH in $(find images -name "*.jpeg"); do
     mkdir -p "$THUMB_DIR/$PHOTO_DIR"
     DESTINATION_FILE="$THUMB_DIR/$PHOTO_FILEPATH"
     if [ ! -f "$DESTINATION_FILE" ]; then
-      echo "$THUMB_DIR/$PHOTO_DIR"
-      convert -resize 320x320^ -gravity center -extent 300x300 $PHOTO_FILEPATH $DESTINATION_FILE
+      echo $DESTINATION_FILE
+      if [ $THUMB_DIR = "thumbs" ]; then
+        convert -resize 320x320^ -gravity center -extent 300x300 $PHOTO_FILEPATH $DESTINATION_FILE
+      else
+        convert -resize 1000x700^ $PHOTO_FILEPATH $DESTINATION_FILE
+      fi
     fi
   done
 done
